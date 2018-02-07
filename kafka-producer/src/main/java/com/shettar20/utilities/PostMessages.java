4b72189a5f2e969f197c303eefeb81306cea7b60
shettar20 <shettar20@gmail.com>
2018-02-07 17:29:27 +0530

    Setup

---
+++ kafka-producer/src/main/java/com/shettar20/utilities/PostMessages.java
@@ -0,0 +1,17 @@
+package com.shettar20.utilities;
+
+import java.util.Properties;
+
+public class PostMessages {
+
+	Properties properties;
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
